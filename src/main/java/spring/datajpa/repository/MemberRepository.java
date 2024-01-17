ff3f7fd89a7392d9b02fac34ea445033867d2a64
ro-el-c <chung3286@naver.com>
2024-01-17 15:52:54 +0900

    feat: 쿼리 메서드 - 메서드 이름으로 NamedQuery 호출

--- src/main/java/spring/datajpa/repository/MemberRepository.java
+++ src/main/java/spring/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package spring.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.datajpa.entity.Member;
 
 import java.util.List;
@@ -10,5 +12,8 @@ import java.util.List;
  * */
 public interface MemberRepository extends JpaRepository<Member, Long> {//JpaRepository<T, ID>
     List<Member> findByNameAndAgeGreaterThan(String name, int age);
+
+//    @Query(name = "Member.findByName") // 없어도 잘 동작함
+    List<Member> findByName(@Param("name") String name);
 
 }
