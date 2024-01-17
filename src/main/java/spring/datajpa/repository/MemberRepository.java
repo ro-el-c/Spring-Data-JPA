772d6dc0bd2bb3f945afb2a12a25332fb4c4e7c4
ro-el-c <chung3286@naver.com>
2024-01-17 15:35:31 +0900

    feat: 쿼리 메서드 - 메서드 이름으로 쿼리 생성

--- src/main/java/spring/datajpa/repository/MemberRepository.java
+++ src/main/java/spring/datajpa/repository/MemberRepository.java
@@ -3,8 +3,12 @@ package spring.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.datajpa.entity.Member;
 
+import java.util.List;
+
 /**
  * Spring Data JPA Repository
  * */
 public interface MemberRepository extends JpaRepository<Member, Long> {//JpaRepository<T, ID>
+    List<Member> findByNameAndAgeGreaterThan(String name, int age);
+
 }
