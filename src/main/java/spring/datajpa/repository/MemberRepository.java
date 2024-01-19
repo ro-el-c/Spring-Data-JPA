92de0566034f45a27dabc7a8e2e4c1277fbafe77
ro-el <chung3286@naver.com>
2024-01-19 19:22:38 +0900

    feat: fetch join 쿼리 직접 작성

--- src/main/java/spring/datajpa/repository/MemberRepository.java
+++ src/main/java/spring/datajpa/repository/MemberRepository.java
@@ -50,4 +50,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {//JpaRepo
     @Query("update Member m set m.age = m.age + 1 " +
             "where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
 }
